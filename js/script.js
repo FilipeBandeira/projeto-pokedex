52a5a67e1192be383e7e4072b6b93ada0f416bab
Ana Luisa Bandeira <filipebandeira2022@gmail.com>
2023-05-03 12:37:35 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     
     return `
         <li class="pokemons">
-            <span class="number">#001</span>
+            <span class="number"># ${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
         
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypesToLi(pokemon.types).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
             </div>
-        </li> `
-
+        </li> 
+        `
 }
 
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
 
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('') /*Forma simplificada de demonstrar um obejto sendo convertido em uma string e em seguida em um novo HTML*/
 })
